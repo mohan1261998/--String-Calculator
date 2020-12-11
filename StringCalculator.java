e74658c80a37cf31dd2fd1086cc49d4ab9899eb1
mohan1261998 <mohan@myanatomy.in>
2020-12-11 10:53:50 +0530

    added all required test cases

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,47 +1,56 @@
 
 
 public class StringCalculator {
-			static int count = 0;
+	static int count = 0;
 
-			public static int GetCalledCount() {
-				return count;
-			}
+	public static int GetCalledCount() {
+		return count;
+	}
 
-			public static int Add(String s) {
-				count = count + 1;
-				int i = 0, sum1 = 0;
-				boolean isNegative = false;
-				String str = "0";
-				String errorMessage = "";
+	public static int Add(String s) {
+		count = count + 1;
+		int i = 0, sum1 = 0;
+		boolean isNegative = false;
+		String str = "0";
+		String errorMessage = "";
 
-				while (i < s.length()) {
-					str = "0";
-					while (i < s.length() && (Character.isDigit(s.charAt(i)) || s.charAt(i) == '-')) {
-						if (str == "0") {
-							str = "" + s.charAt(i);
-						} else {
-							str = str + s.charAt(i);
-						}
-						i = i + 1;
-					}
-					int n = Integer.parseInt(String.valueOf(str));
-					if (n < 0) {
-						isNegative = true;
-						errorMessage = errorMessage + n + " , ";
-					} else if (n < 1000) {
-						sum1 = sum1 + n;
-					}
-					i = i + 1;
+		while (i < s.length()) {
+			str = "0";
+			while (i < s.length() && (Character.isDigit(s.charAt(i)) || s.charAt(i) == '-')) {
+				if (str == "0") {
+					str = "" + s.charAt(i);
+				} else {
+					str = str + s.charAt(i);
 				}
-				if (isNegative) {
-					throw new IllegalArgumentException("Negative numbers are not allowed. " + errorMessage);
-				}
-				return sum1;
+				i = i + 1;
 			}
-
-			public static void main(String args[]) {
-				int sum1 = Add("//[***]\n1***2***3000");
-				int count = GetCalledCount();
-				System.out.println("Sum is " + sum1 + " and count is " + count);
+			int n = Integer.parseInt(String.valueOf(str));
+			if (n < 0) {
+				isNegative = true;
+				errorMessage = errorMessage + n + " , ";
+			} else if (n < 1000) {
+				sum1 = sum1 + n;
 			}
+			i = i + 1;
 		}
+		if (isNegative) {
+			throw new IllegalArgumentException("Negative numbers are not allowed. " + errorMessage);
+		}
+		return sum1;
+	}
+
+	public static void main(String args[]) {
+		int count = GetCalledCount();
+		System.out.println("Sum is " + Add(""));
+		System.out.println("Sum is " + Add("1"));
+		System.out.println("Sum is " + Add("1,2"));
+		System.out.println("Sum is " + Add("1\n2,3"));
+		System.out.println("Sum is " + Add("//;\n1;2"));
+		// System.out.println("Sum is " + Add("-1,-2,-3,9"));
+		System.out.println("Sum is " + Add("1,1001,1,2000"));
+		System.out.println("Sum is " + Add("//[***]\n1***2***3"));
+		System.out.println("Sum is " + Add("//[*][%]\n1*2%3"));
+		System.out.println("Sum is " + Add("//[**][%%]\n1**2%%3"));
+		System.out.println("count is " + Add("count is="+GetCalledCount()));
+	}
+}
